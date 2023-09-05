eb13264b958889f0bfac9447bd06eb9f49abc84f
Elise Chavez <elisechavez23@gmail.com>
2023-09-05 18:57:08 +0000

    undoing changes, there must have been a bug

--- src/htcdaskgateway/__init__.py
+++ src/htcdaskgateway/__init__.py
@@ -1,5 +1,5 @@
-from cluster import HTCGatewayCluster 
-from gateway import HTCGateway 
+from .cluster import HTCGatewayCluster 
+from .gateway import HTCGateway 
 
 # @author Maria A. - mapsacosta
 
