0614129de3ca3d7b70f30e0b8f1679ecb7602cd8
elisec <elisechavez23@gmail.com>
2024-04-23 19:52:19 +0000

    Fixed issue with the image not being picked up by the object

--- src/htcdaskgateway/cluster.py
+++ src/htcdaskgateway/cluster.py
@@ -23,7 +23,8 @@ class HTCGatewayCluster(GatewayCluster):
     def __init__(self, **kwargs):
         self.scheduler_proxy_ip = kwargs.pop('', '131.225.218.222')
         self.batchWorkerJobs = []
-        self.cluster_options = kwargs.get('cluster_options','image')
+        self.cluster_options = kwargs.get('cluster_options', 'image')
+        print(self.cluster_options.image)
         if self.cluster_options.image == None:
             self.cluster_options.image = 'coffeateam/coffea-dask-cc7-gateway:0.7.21-py3.10-g7cbcc'
             print("Selected Image: ", self.cluster_options.image)
